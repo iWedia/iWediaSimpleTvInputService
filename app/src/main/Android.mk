864b02d1adffec4f2d5027f5423804efc1d37f1c
Nikola Vranic <Nikola.Vranic@rt-rk.com>
2016-01-11 15:40:29 +0100

    Add support for Andorid build system
    With this changes iWedia TV Input Example can be built from AOSP.

---
+++ app/src/main/Android.mk
@@ -0,0 +1,18 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_PACKAGE_NAME := iWediaSimpleTvInputService
+
+LOCAL_SRC_FILES := $(call all-java-files-under, java)
+LOCAL_SRC_FILES += $(call all-Iaidl-files-under, java)
+
+LOCAL_STATIC_JAVA_LIBRARIES := com.iwedia.dtv.framework.service
+
+LOCAL_PROGUARD_ENABLED := disabled
+
+LOCAL_DEX_PREOPT := false
+
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_PACKAGE)
